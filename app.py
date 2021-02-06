70341077d38182488df112a2961919b73f0ed397
Usha Saravanakumar <ushaakumaar@gmail.com>
2021-02-05 23:16:14 -0500

    Code for Flask Setup and accessing environment variables

--- app.py
+++ app.py
@@ -9,3 +9,11 @@ import os
 from config import CSV_FILE_URL
 import pandas as pd
 
+###############################################
+# Flask Setup & Access Environ Variables
+###############################################
+
+# Creating new application of the flask module
+app = Flask(__name__)
+
+csv_file_path = environ.get('CSV_FILE_PATH') or CSV_FILE_URL
