3d9fdfe3c9d28426f9efbffcea823d04412de0fc
Usha Saravanakumar <ushaakumaar@gmail.com>
2021-02-06 00:18:59 -0500

    Code to select all required html elements using d3.js

---
+++ static/js/data.js
@@ -0,0 +1,6 @@
+/***************************************************
+SELECT ALL REQUIRED HTML ELEMENTS
+****************************************************/
+
+var tableBody = d3.select("tbody");
+var fetching = d3.select("#fetching");
